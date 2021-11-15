8745aacdd8415ce39e6b1e3399125f341ecbec42
yang12327 <yang12327@Gmail.com>
2021-11-15 17:10:08 +0800

    建立篩選器

--- content.js
+++ content.js
@@ -1,19 +1,32 @@
 let A = null;
-$(() => { Load(null) })
-function Load(res) { //載入頁面
+$(() => { Load() })
+function Load() { //載入頁面
   A = JSON.parse(localStorage.getItem("AutoVerify" + location.pathname));
   if (A == null) return "未設定任何參數";
   let elem = [];
   for (let i = 1; i <= 2; i++) {
-    let e = A[i];
-    if (e == null) return "參數未設定完全";
-    let t = $(e.Tag);
-    if (t == null || t.length == 0) return "找不到物件";
-    t = t.get(t.length > 1 ? e.index : 0);
-    if (t == null) return "找不到物件";
-    elem.push(t);
+    let e = FindElem(i);
+    if (e.obj == null) return e.err;
+    elem.push(e.obj);
   }
-  img2txt(elem[0], A[0], elem[1], res);
+  img2txt(elem[0], A[0], elem[1]);
+  if (A[8] != null) {
+    let t = FindElem(8).obj;
+    if (t != null)
+      $(t).on("click", () => {
+        setTimeout(Load, 200);
+      });
+  }
+}
+
+function FindElem(i) {
+  let e = A[i];
+  if (e == null) return { err: "參數未設定完全" };
+  let t = $(e.Tag);
+  if (t == null || t.length == 0) return { err: "找不到物件" };
+  t = t.get(t.length > 1 ? e.index : 0);
+  if (t == null) return { err: "找不到物件" };
+  return { obj: t };
 }
 
 let Mode = 0;
@@ -42,7 +55,7 @@ function set(sender) {  //按下元素
   else if (Info.Tag == null || Info.index == -1)
     return;
   console.log(Info);
-  if (A == null) A = [1, null, null];
+  if (A == null) A = [1];
   A[Mode] = Info;
   localStorage.setItem("AutoVerify" + location.pathname, JSON.stringify(A));
   Mode = 0;
@@ -52,6 +65,14 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   let i = 0;
   let res = null;
   switch (request.info) {
+    case 6: //篩選器
+      console.log(request);
+      if (request.index != null && request.checked != null) {
+        A[request.index] = request.checked;
+        localStorage.setItem("AutoVerify" + location.pathname, JSON.stringify(A));
+      }
+      break;
+
     case 5: //測試
       console.log(sendResponse);
       if ((res = Load(sendResponse)) != null)
@@ -61,45 +82,53 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     case 4: //模式B
       i = 1;
     case 3: //模式A
-      if (A == null) A = [1, null, null];
+      if (A == null) A = [];
       A[0] = i + 1;
       localStorage.setItem("AutoVerify" + location.pathname, JSON.stringify(A));
       console.log(A);
       break;
 
+    case 8: //設定重新產生
+      i = 7;
     case 2: //設定輸入框
-      i = 1;
-    // SetAutoVerify(1);
-    // break;
+      if (i == 0) i = 1;
     case 1: //設定驗證碼
-      // SetAutoVerify(2);
       $(":visible").on("click", function () { set(this) }); //在元素上貼標籤
       Mode = i + 1;
       break;
 
-    case 0: //點開
+    case 0: //點開插件
       res = {};
       if (A != null) res.engine = A[0] == 1 ? "A模式" : "B模式";
+      else break;
       if (A[1] != null) res.img = "更換驗證碼圖片(已設定)";
       if (A[2] != null) res.box = "更換回答輸入框(已設定)";
+      if (A[8] != null) res.ren = "更換重新產生按鈕(已設定)";
+      res.filter = [A[3] != false, A[4] != false, A[5] != false, A[6] == true];
+      res.word = (A[7] == null ? 4 : A[7]);
       sendResponse(res);
       break;
 
     default:
-      console.log(request.info)
-      // sendResponse('我收到了你的情书，popup~');
+      console.log(request);
       break;
   }
-})
+});
 
-function img2txt(img, engine, elem, res) {
+function send(message) {
+  chrome.runtime.sendMessage(message, res => {
+    if (res != null) console.log(res);
+  });
+}
+
+function img2txt(img, engine, elem) {
   const canvas = document.createElement('canvas');
   const ctx = canvas.getContext('2d');
   canvas.width = img.width;
   canvas.height = img.height;
   ctx.drawImage(img, 0, 0);
   let b64img = canvas.toDataURL('image/jpeg');
-  if (res != null) res(b64img);
+  send({ info: 5, img: b64img });
   console.log(b64img);
   $.ajax({
     "url": "https://api.ocr.space/parse/image",
@@ -118,13 +147,33 @@ function img2txt(img, engine, elem, res) {
     }
   }).done(r => {
     if (r.OCRExitCode == 1) {
-      if (res != null) alert("辨識結果：『" + r.ParsedResults[0].ParsedText + "』");
-      console.log(r.ParsedResults[0].ParsedText);
-      $(elem).val(r.ParsedResults[0].ParsedText);
+      let OCR = Filter(r.ParsedResults[0].ParsedText);
+      send({ info: 5, text: OCR });
+      console.log(OCR);
+      $(elem).val(OCR);
     }
     else {
-      if (res != null) alert("辨識失敗\n錯誤訊息：" + r.ErrorMessage);
+      send({ info: 5, err: r.ErrorMessage });
       console.log(r);
     }
   });
+}
+
+function Filter(text) {
+  console.log("In:", text);
+  let OCR = text.match(/\w/g);
+  if (OCR == null) return "";
+  OCR = OCR.join("");
+  let Up = A[3] != false, Do = A[4] != false, Di = A[5] != false;
+  if (!Up && Do) OCR = OCR.replaceAll("I", "l").toLowerCase();
+  else if (Up && !Do) OCR = OCR.replaceAll("l", "I").toUpperCase();
+  else if (!Up && !Do) OCR = OCR.replaceAll(/o/gi, "0").replaceAll(/[lI]/g, "1");
+  if (!Di) {
+    OCR = OCR.replaceAll("0", "O").replaceAll("1", Up ? "I" : "l");
+    if (!Up) OCR = OCR.toLowerCase();
+  }
+  console.log("Convert:", OCR);
+  OCR = OCR.match(new RegExp("[" + (Up ? "A-Z" : "") + (Do ? "a-z" : "") + (Di ? "0-9" : "") + "]", 'g')).join("");
+  console.log("Filter:", OCR);
+  return OCR;
 }
--- popup.js
+++ popup.js
@@ -10,14 +10,21 @@ $("[engine]").on('click', function () { //切換引擎
   $("#engine").text($(this).text().substr(0, 1) + "模式");
 });
 
-function send(Info) {
+$("input[type=checkbox]").change(function () {
+  send(6, { index: parseInt($(this).attr("index")), checked: $(this).prop("checked") });
+});
+
+$("#word").change(function () {
+  send(6, { index: 7, checked: $(this).val() });
+  $("[index=6]").prop("checked", true).change();
+});
+
+function send(Info, message = {}) {
   chrome.tabs.query({
     active: true,
     currentWindow: true
   }, (tabs) => {
-    let message = {
-      info: Info
-    }
+    message.info = Info;
     chrome.tabs.sendMessage(tabs[0].id, message, res => {
       if (res == null) return;
       switch (message.info) {
@@ -28,10 +35,13 @@ function send(Info) {
             $("[send=1]").text(res.img);
           if (res.box != null)
             $("[send=2]").text(res.box);
-          break;
-
-        case 5:
-          $("#TestImg").attr("src", res);
+          if (res.ren != null)
+            $("[send=8]").text(res.ren);
+          $("#Up").prop("checked", res.filter[0]);
+          $("#Do").prop("checked", res.filter[1]);
+          $("#Di").prop("checked", res.filter[2]);
+          $("#W").prop("checked", res.filter[3]);
+          $("#word").val(res.word);
           break;
 
         default:
@@ -41,3 +51,18 @@ function send(Info) {
     })
   })
 }
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  switch (request.info) {
+    case 5:
+      if (request.img != null) $("#TestImg").attr("src", request.img);
+      if (request.text != null) $("#TestResult").text("辨識結果：『" + request.text + "』");
+      if (request.err != null) $("#TestError").text("辨識失敗：" + request.err);
+      break;
+
+    default:
+      request.from = sender.tab ? "content >" + sender.tab.url : "background"
+      sendResponse(request);
+      break;
+  }
+});

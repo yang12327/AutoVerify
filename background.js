fca14a793222fcea060e837dc229a7b71c23fdeb
yang12327 <yang12327@Gmail.com>
2021-11-20 13:05:41 +0800

    test

--- background.js
+++ background.js
@@ -1,11 +1,19 @@
 let C = 0;
+chrome.storage.local.get("C", r => {
+    C = r.C;
+    if (C == null) C = 0;
+})
 // setInterval(() => {
 //     console.log(C++);
 // }, 1000);
 
 
 chrome.alarms.onAlarm.addListener(alarm => {
+    if (alarm.name == "counter") {
+        chrome.tabs.create({ url: "https://www.google.com/" + alarm.name + C })
+    }
     console.log(C++, alarm, new Date());
+    chrome.storage.local.set({ C: C });
 });
 
-chrome.alarms.create("counter", { delayInMinutes: 1, periodInMinutes: 1 });
+// chrome.alarms.create("counter", { delayInMinutes: 1, periodInMinutes: 1 });
